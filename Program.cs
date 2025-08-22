32e665a5998d15d75b49d924d8ea94d40dff6289
Carolina Passoni <105358062+CarolinaPassoni@users.noreply.github.com>
2025-08-22 15:12:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Sistema de Avaliação de Alunos ===");
+
+        Console.Write("Digite o nome do aluno: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite a primeira nota: ");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a segunda nota: ");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        double media = (nota1 + nota2) / 2;
+
+        Console.WriteLine($"\nAluno: {nome}");
+        Console.WriteLine($"Média: {media}");
+
+        if (media >= 7)
+        {
+            Console.WriteLine("Situação: Aprovado ");
+        }
+        else if (media >= 5)
+        {
+            Console.WriteLine("Situação: Recuperação ");
+        }
+        else
+        {
+            Console.WriteLine("Situação: Reprovado ");
+        }
+    }
+}
